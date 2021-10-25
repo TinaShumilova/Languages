375df43d4087ef2828eea4ff195c321fbb5f4a18
Tina Shumilova <shutiva@gmail.com>
2021-10-25 16:41:02 +0300

    Example 006 Base

---
+++ Example_006_Base/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 9;
+int b = 6;
+int c = 3;
+int d = 8;
+int e = 3;
+int max = a;
+
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+
+Console.Write("Max = ");
+Console.WriteLine(max);
