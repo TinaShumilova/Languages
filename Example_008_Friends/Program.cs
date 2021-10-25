4a2bd3e0476412310abcc6372c970139a11267e7
Tina Shumilova <shutiva@gmail.com>
2021-10-25 18:09:26 +0300

    Example 008 friends

---
+++ Example_008_Friends/Program.cs
@@ -0,0 +1,28 @@
+﻿int count = 0;
+double distance = 10000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+bool friend = false;
+double time = 0;
+
+while(distance > 10)
+{
+    if(friend == false)
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friend = true;
+        distance = distance - (firstFriendSpeed + secondFriendSpeed)*time;
+        count = count + 1;
+    }
+    else
+    {
+        time = distance / (secondFriendSpeed + dogSpeed);
+        friend = false;
+        distance = distance - (firstFriendSpeed + secondFriendSpeed)*time;
+        count = count + 1;
+    }
+}
+Console.Write("Собака пробежала ");
+Console.Write(count);
+Console.WriteLine(" раз."); 
