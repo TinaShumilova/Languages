d6700648eaf084690306e12ca98c0ac1ecec54e8
Tina Shumilova <shutiva@gmail.com>
2021-10-29 18:17:35 +0300

    correction function

--- Example_009_Function/Program.cs
+++ Example_009_Function/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 56;
+int b1 = 74;
+int c1 = 86;
+int a2 = 26;
+int b2 = 2432;
+int c2 = 23;
+int a3 = 5456;
+int b3 = 69;
+int c3 = 71;
+
+//int max = a1;
+//if(b1 > max) max = b1;
+//if(c1 > max) max = c1;
+
+//if(a2 > max) max = a2;
+//if(b2 > max) max = b2;
+//if(c2 > max) max = c2;
+
+//if(a3 > max) max = a3;
+//if(b3 > max) max = b3;
+//if(c3 > max) max = c3;
+
+//int max1 = Max(a1, b1, c1);
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
+//int max = Max(max1, max2, max3);
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+
+Console.WriteLine(max);
