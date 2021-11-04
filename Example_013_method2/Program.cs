f9cd21eb18568d779bd6af2042675532574fcc26
Tina Shumilova <shutiva@gmail.com>
2021-11-04 13:34:34 +0200

    create Method21

--- Example_013_method2/Program.cs
+++ Example_013_method2/Program.cs
@@ -4,3 +4,16 @@
 }
 
 Method2("Текст сообщения");
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while(i < count)
+    {
+         Console.WriteLine(msg);
+         i++;
+    }
+    
+}
+
+Method21("Текст", 3);
