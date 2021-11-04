34f00661d12287e1221f1762613854de8c6d1894
Tina Shumilova <shutiva@gmail.com>
2021-11-04 15:02:28 +0200

    create For for text

--- Example_016_For/Program.cs
+++ Example_016_For/Program.cs
@@ -13,6 +13,7 @@ string res = Method4(10, "z");
 Console.WriteLine(res);
 */
 
+//Таблица умножения (цикл в цикле)
 /* 
 for(int i = 2; i < 10; i++)
 {
@@ -24,3 +25,45 @@ for(int i = 2; i < 10; i++)
 }
 */
 
+/*
+========Работа с текстом=============
+Дан текст. В тексте все пробелы нужно заменить чёрточками,
+маленькие буквы "к" заменить большими "К",
+а большие "С" заменить маленькими "с".
+*/
+
+string text = "Миры появляются и исчезают, отбрасывают тени и отражения, которые,"
+            + " в свою очередь порождают тени теней и отражения отражений. "
+            + " И во всех мирах кипит война и льется кровь. Разумные (как смертные"
+            + " так и бессмертные) делят земли, нефть, уран... да мало ли чего еще."
+            + " И даже если всего достаточно - продолжают сражаться за Веру. Слишком"
+            + " велик соблазн определять, что есть Свет и вести к этому Свету других."
+            + " И потому вновь и вновь катятся кости брошенных жребиев. Идет Игра...";
+
+Console.WriteLine(text);
+Console.WriteLine();
+
+//string text = "qwerty"
+//               012345
+//text[3] // r 
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int lenght = text.Length;
+    for(int index = 0; index < lenght; index++)
+    {
+        if(text[index] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[index]}";
+    }
+    return result;
+}
+
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'С', 'с');
+Console.WriteLine(newText);
