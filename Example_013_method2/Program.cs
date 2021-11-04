c3ca61fc17b098794c471078131e97de6bf42fc2
Tina Shumilova <shutiva@gmail.com>
2021-11-04 13:21:23 +0200

    add Method2

--- Example_013_method2/Program.cs
+++ Example_013_method2/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+Method2("Текст сообщения");
