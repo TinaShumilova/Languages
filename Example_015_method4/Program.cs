0d8c005ebd5d89accbfef97222929e9a7e430cfa
Tina Shumilova <shutiva@gmail.com>
2021-11-04 14:05:38 +0200

    create Method4

--- Example_015_method4/Program.cs
+++ Example_015_method4/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+    while(i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+
+string res = Method4(10, "z");
+Console.WriteLine(res);
