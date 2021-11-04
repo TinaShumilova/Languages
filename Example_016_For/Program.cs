b83d3b6345eff4b7f6da6835d9bca8c8417c1620
Tina Shumilova <shutiva@gmail.com>
2021-11-04 14:20:44 +0200

    create For

--- Example_016_For/Program.cs
+++ Example_016_For/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+string Method4(int count, string text)
+{
+    string result = String.Empty;
+    for(int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+string res = Method4(10, "z");
+Console.WriteLine(res);
+*/
+
+/* 
+for(int i = 2; i < 10; i++)
+{
+    for(int j = 2; j < 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i*j}");
+    }
+    Console.WriteLine();
+}
+*/
+
